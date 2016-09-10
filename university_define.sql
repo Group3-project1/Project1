7912dfb6324d916fcdc9fca332195a086f426f3b
tonim1113 <mtz.toni.e@gmail.com>
2016-09-10 16:07:39 -0500

    Update university_define.sql

--- university_define.sql
+++ university_define.sql
@@ -6,5 +6,20 @@ USE toni_university; /* change name for running script */
 
 DROP TABLE IF EXISTS person, course, class_section, enrollment, prerequisite;
 
-CREATE TABLE person(ID INT(9), FName_LName VARCHAR(25), class_standing VARCHAR(2), major VARCHAR(10),PRIMARY KEY(ID));
+CREATE TABLE person
+(
+ID INT NOT NULL AUTO_INCREMENT, 
+FName_LName VARCHAR(25), 
+class_standing VARCHAR(10), 
+major VARCHAR(10),
+PRIMARY KEY(ID));
 
+ALTER TABLE person AUTO_INCREMENT=111000;
+
+CREATE TABLE course 
+(
+course_number VARCHAR(8), 
+course_name VARCHAR(25), 
+credit_hours char(1), 
+department VARCHAR(10), 
+PRIMARY KEY(course_number));
