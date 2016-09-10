ea49ee19e03045e405a92a7f672ebf1d0b418883
tonim1113 <mtz.toni.e@gmail.com>
2016-09-10 16:40:52 -0500

    Update university_populate.sql

--- university_populate.sql
+++ university_populate.sql
@@ -1,1 +1,11 @@
 /* this will be the insert statements */
+
+use /* your database name */;
+
+
+--Poplulates course table
+INSERT INTO course(course_number, course_name, credit_hours, department) VALUES
+('CS1310', 'Intro to Computer Science', 4, 'CS'),
+('CS3320', 'Data Structures', 4, 'CS'),
+('MATH2410', 'Discrete Mathematics', 4, 'MATH'),
+('CS3380', 'Database', 4, 'CS');
