4294e9d6c8fa41ae77a2433a1a8b543697c898b3
tonim1113 <mtz.toni.e@gmail.com>
2016-09-11 16:07:04 -0500

    Update university_populate.sql

--- university_populate.sql
+++ university_populate.sql
@@ -1,13 +1,5 @@
 /* this will be the insert statements */
 
-use /* your database name */;
-
-/*Populates person table with 100 random names. When I did my database I added Bio as a major. I don't know if you guys whant to stick with that
-or you gauys want to change it. My other tables also have bio classes and bio sections, including the enrollment table. 
--- jorge
-*/
-
-
 use toni_university/* your database name */;
 
 /*Populates person table with 100 random names. When I did my database I added Bio as a major. I don't know if you guys whant to stick with that
