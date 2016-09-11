e01ed1d4e1ff10d45168c95a5da0a81a7d9e59ba
jorgeduardos <jorgeduardo.1994@gmail.com>
2016-09-11 09:37:34 -0500

    Update university_define.sql

--- university_define.sql
+++ university_define.sql
@@ -23,3 +23,28 @@ course_name VARCHAR(25),
 credit_hours VARCHAR(2),  --Better VARCHAR than INT?
 department VARCHAR(10), 
 PRIMARY KEY(course_number));
+
+CREATE TABLE section --jorge
+(
+section_identifier int(10), 
+course_number varchar(15), 
+semester varchar(10), 
+year varchar(4), 
+instructor_id int(9), 
+primary key(section_identifier), 
+foreign key(instructor_id) references persons(ID));
+
+CREATE TABLE enrollment --jorge
+(
+student_id int(9), 
+section_identifier int(10),
+grade char(3), 
+foreign key(student_id) references persons(ID),
+foreign key(section_identifier) references section(section_identifier));
+
+CREATE TABLE prereq --jorge
+(
+course_number varchar(15),
+prerequisite_number varchar(15),
+foreign key(course_number) references course(course_number),
+foreign key(prerequisite_number) references course(course_number));
