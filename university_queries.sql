d3f112eacaa4db58b448ba541ea56b93e13c00d3
clauderbn <claude.robin101@gmail.com>
2016-09-12 18:37:07 -0500

    Update university_queries.sql

--- university_queries.sql
+++ university_queries.sql
@@ -12,9 +12,10 @@ SELECT instructor_id FROM class_section WHERE section_identifier IS NOT NULL;
 /*3. List the names of all instructors. */
 /*SELECT Name FROM Person WHERE Class = '00';*/
 SELECT Fname_Lname FROM person WHERE class_standing = 'Instructor';
-/*4. Query 4, section iden */   
-SELECT Fname_Lname FROM person INNER JOIN enrollment ON person.ID = enrollment.student.id WHERE secton_identifier = ‘119’;
-/*5 Query 5 */
-SELECT student_id FROM enrollment WHERE grade IS NULL;
-/*6 Query 6 */
+/* Query 4-6  -Claude */
+/*4. Query 4, List the names of all students in a course section by using the format MATH2410 or any other course  */   
+SELECT FName_LName FROM person INNER JOIN enrollment ON person.ID = enrollment.student_id INNER JOIN class_section ON enrollment.section_identifier = class_section.section_identifier WHERE course_numb = 'MATH2410';
+/*5 Query 5 List student Names who have not received a grade */
+SELECT FName_LName FROM person INNER JOIN enrollment ON enrollment.student_id = person.ID WHERE grade IS NULL;
+/*6 Query 6 couts the amount of students in each section. */
 SELECT section_identifier, COUNT(*) FROM enrollment GROUP BY section_identifier;
