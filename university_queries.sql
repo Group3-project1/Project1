3be819f30b106bb3e89bdb5c09e61ee4aacd81e8
clauderbn <claude.robin101@gmail.com>
2016-09-12 14:09:41 -0500

    Update university_queries.sql

--- university_queries.sql
+++ university_queries.sql
@@ -12,5 +12,9 @@ SELECT instructor_id FROM class_section WHERE section_identifier IS NOT NULL;
 /*3. List the names of all instructors. */
 /*SELECT Name FROM Person WHERE Class = '00';*/
 SELECT Fname_Lname FROM person WHERE class_standing = 'Instructor';
-/*4. Query 4 */   
+/*4. Query 4, section iden */   
 SELECT Fname_Lname FROM person INNER JOIN enrollment ON person.ID = enrollment.student.id WHERE secton_identifier = ‘MATH2410’;
+/*5 Query 5 */
+SELECT student_id FROM enrollment WHERE grade IS NULL;
+/*6 Query 6 */
+SELECT section_identifier, COUNT(*) FROM enrollment GROUP BY section_identifier;
